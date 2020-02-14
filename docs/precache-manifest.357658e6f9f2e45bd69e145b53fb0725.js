21a7f409e77f896c6a2cecaf501ca421935daee8
shihuairenai <https://git.hypers.com/frontend-web/unilever-fe.git>
2020-02-14 16:07:58 +0800

    n

---
+++ docs/precache-manifest.357658e6f9f2e45bd69e145b53fb0725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bde7bfba9727a6f5f005d4d728f0c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b076abf592a21b2d0e4a",
+    "url": "./static/css/main.c32f2d25.chunk.css"
+  },
+  {
+    "revision": "853af04a51e90d0fac73",
+    "url": "./static/js/2.31acad31.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "./static/js/2.31acad31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b076abf592a21b2d0e4a",
+    "url": "./static/js/main.d7296fa2.chunk.js"
+  },
+  {
+    "revision": "343552b668ab6eca28bc",
+    "url": "./static/js/runtime-main.529b5a57.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0bdfcdd4d8faab72afe7d36a4e7a1367.js"
+  "./precache-manifest.357658e6f9f2e45bd69e145b53fb0725.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
